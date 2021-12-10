16e33fc30534689a6da6398cc29dd824daa40ac1
NarotKars <garabediangars@gmail.com>
2021-12-10 21:47:19 +0400

    allow cors

--- Badges/Startup.cs
+++ Badges/Startup.cs
@@ -62,6 +62,11 @@ namespace Badges
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseAuthentication();
